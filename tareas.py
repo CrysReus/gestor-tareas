39d6ad638d18756da7732786ecca8208d316cdb8
cristian <cristianestudios334@outlook.es>
2025-04-29 23:45:53 +0200

    añadiendo una nueva funcion listar tareas a tareas.py

--- tareas.py
+++ tareas.py
@@ -1,3 +1,12 @@
 def agregar_tarea(lista, tarea):
  lista.append(tarea)
  return lista
+
+
+def listar_tareas(lista):
+ if not lista:
+  print("No hay tareas pendientes.")
+ else:
+  print("Lista de tareas:")
+  for i, tarea in enumerate(lista, start=1):
+   print(f"{i}. {tarea}")
